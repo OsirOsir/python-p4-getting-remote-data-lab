f20b0ed4d79f1c2fb096d474bb174fbb57450009
Philip osir Otieno <philiposir2@gmail.com>
2024-10-05 06:54:50 +0300

    updates

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,26 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()
+            return response.content
+        except requests.RequestException as e:
+            print(f"An error occurred: {e}")
+            return None
 
     def load_json(self):
-        pass
+        response = requests.get(self.url)
+        return response.json()
+        
+    
+    # def load_json(self):
+    #     response = self.get_response_body()
+    #     if response is not None:
+    #         try:
+    #             return response.json()
+    #         except json.JSONDecodeError:
+    #             print("Error decoding JSON")
+    #             return None
+            
+    #     return None
